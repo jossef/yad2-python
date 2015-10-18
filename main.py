b37fe395c08b5b51326066c4fb5e8ac200bef06b
Jossef Harush <jossef12@gmail.com>
2015-10-18 23:38:19 +0300

    some testing, this is the minimal code you can get

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from crawler            import Yad2Crawler
+import requests
+import uuid
 
 
 def main():
-    crawler = Yad2Crawler()
-    crawler.crawl()
+    identifier = uuid.uuid4()
+
+    params = {
+        'CatID': 1,
+        'Auto': 0.0,
+        'SubModelID': 247.0,
+        'SubCatID': 1,
+        'ModelID': 28.0,
+        'ToPrice': 35000,
+        'FromYear': 2009.0,
+        'AppType': 'Android',
+        'AppVersion': 2.9,
+        'DeviceType': 'Nexus 5',
+        'udid': identifier,
+        'OSVersion': 5.1
+    }
+
+    headers = {
+        'User-Agent': 'Apache-HttpClient/UNAVAILABLE (java 1.4)'
+    }
+
+    r = requests.get('http://m.yad2.co.il/API/MadorResults.php', params=params, headers=headers)
+
+    print r.content
+
 
 if __name__ == "__main__":
     main()
